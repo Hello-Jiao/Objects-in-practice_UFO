c609453dc3c448caf0472bbf3e8305133845c1b2
Jiao Wang <jiao228228@gmail.com>
2020-10-18 23:47:58 +0200

    add functions

--- sketch.js
+++ sketch.js
@@ -8,46 +8,7 @@ function setup()
     createCanvas(800,600);
     noStroke();
 
-
-
-    flying_saucer = {
-        x: 400,
-        y: 150,
-        width: 150,
-        height: 50,
-        window_width: 0.75,
-        window_height: 0.85,
-        base_height: 0.45,
-        num_lights: 10,
-        brightnesses:[],
-        beam_on: false,
-
-        hover: function()
-        {
-            console.log("hover");
-            this.x += random(-1,1);
-            this.y += random(-1,1);
-        },
-
-        beam: function()
-        {
-            fill(255,255,0,120);
-
-            if(random() > 0.2)
-            {
-            beginShape();
-            vertex(this.x - this.width * 0.25, this.y );
-            vertex(this.x + this.width * 0.25, this.y );
-            vertex(this.x + this.width * 0.55, height - 100 );
-            vertex(this.x - this.width * 0.55, height - 100 );
-            endShape(CLOSE);
-            }
-        }
-    }
-
-    for(var i = 0; i < flying_saucer.num_lights; i++){
-        flying_saucer.brightnesses.push((i * 20)%255);
-    }
+    flying_saucer = new Flying_saucer;
 }
 
 function draw()
@@ -57,58 +18,15 @@ function draw()
     //draw the ground
     fill(0,50,0);
     rect(0,height - 100, width, 100);
+
     
-    if( flying_saucer.beam_on == true)
+    if(flying_saucer.beam_on == true)
     {
         flying_saucer.beam();
     }
-    
-    
-    //draw the flying saucer
-    fill(175,238,238);
-    arc(
-        flying_saucer.x,
-        flying_saucer.y,
-        flying_saucer.width * flying_saucer.window_width,
-        flying_saucer.height * flying_saucer.window_height,
-        PI,TWO_PI);
-    fill(150);
-    arc(
-        flying_saucer.x,
-        flying_saucer.y,
-        flying_saucer.width,
-        flying_saucer.height/2,
-        PI,TWO_PI);
-    fill(50);
-    arc(
-        flying_saucer.x,
-        flying_saucer.y,
-        flying_saucer.width,
-        flying_saucer.height * flying_saucer.base_height,
-        0,PI);
 
-        flying_saucer.hover();
-        
-    
-     //draw the lights
-    var incr = flying_saucer.width/(flying_saucer.num_lights -1);
-
-    for(var i = 0; i < flying_saucer.num_lights; i++){
-        
-        var x = flying_saucer.x - flying_saucer.width/2 + i * incr;
-        fill(flying_saucer.brightnesses[i]);
-        ellipse(
-            x,
-            flying_saucer.y,
-            5,
-            5
-        )
-        flying_saucer.brightnesses[i] += 2;
-        if(flying_saucer.brightnesses[i] > 255)
-        {
-            flying_saucer.brightnesses[i] = 100;
-        }
-    }
+    flying_saucer.draw();
+    flying_saucer.hover();
 }
 
 function keyPressed()
@@ -120,3 +38,88 @@ function keyReleased()
 {
     flying_saucer.beam_on = false;
 }
+
+function Flying_saucer () {
+    this.x = 200;
+    this.y = 100;
+    this.width = 150;
+    this.height = 50;  
+    this.window_width = 0.75;
+    this.window_height = 0.85;
+    this.base_height = 0.45;
+    this.num_lights = 10;
+    this.brightnesses =[];
+    this.beam_on = false;
+
+this.hover = function()
+{
+    this.x += random(-1,1);
+    this.y += random(-1,1);
+},
+
+this.draw = function()
+{
+    //draw the flying saucer
+    fill(175,238,238);
+    arc(
+        this.x,
+        this.y,
+        this.width * this.window_width,
+        this.height * this.window_height,
+        PI,TWO_PI);
+    fill(150);
+    arc(
+        this.x,
+        this.y,
+        this.width,
+        this.height/2,
+        PI,TWO_PI);
+    fill(50);
+    arc(
+        this.x,
+        this.y,
+        this.width,
+        this.height * this.base_height,
+        0,PI);
+        
+    
+     //draw the lights
+    var incr = this.width/(this.num_lights -1);
+
+    for(var i = 0; i < this.num_lights; i++){
+        
+        var x = this.x - this.width/2 + i * incr;
+        fill(this.brightnesses[i]);
+        ellipse(
+            x,
+            this.y,
+            5,
+            5
+        )
+        this.brightnesses[i] += 2;
+        if(this.brightnesses[i] > 255)
+        {
+            this.brightnesses[i] = 100;
+        }
+    }
+}
+
+this.beam = function()
+{
+    fill(255,255,0,120);
+
+    if(random() > 0.2)
+    {
+    beginShape();
+    vertex(this.x - this.width * 0.25, this.y );
+    vertex(this.x + this.width * 0.25, this.y );
+    vertex(this.x + this.width * 0.55, height - 100 );
+    vertex(this.x - this.width * 0.55, height - 100 );
+    endShape(CLOSE);
+    }
+}
+for(var i = 0; i < this.num_lights; i++){
+    this.brightnesses.push((i * 20)%255);
+    }
+}
+
